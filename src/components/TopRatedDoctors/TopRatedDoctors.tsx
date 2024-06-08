3c38e480673360e40fb2aec5ba107a1df3dc9ee2
apurbojamananis <apurbojaman.anis@gmail.com>
2024-06-08 09:55:37 +0600

    added doctors section

--- src/components/TopRatedDoctors/TopRatedDoctors.tsx
+++ src/components/TopRatedDoctors/TopRatedDoctors.tsx
@@ -1,12 +1,25 @@
-import { Box, Container, Typography } from "@mui/material";
-const TopRatedDoctors = () => {
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+const TopRatedDoctors = async () => {
+  const res = await fetch("http://localhost:5000/api/v1/doctor?page=1&limit=3");
+  const { data: doctors } = await res.json();
+
   return (
     <Box
       sx={{
         my: 10,
         py: 30,
         backgroundColor: "rgba(20,20,20,0.1)",
-        clipPath: "polygon(0 0, 100% 40%, 100% 100%, 0 60%)",
+        clipPath: "polygon(0 0, 100% 30%, 100% 100%, 0 70%)",
       }}
     >
       <Box
@@ -24,6 +37,35 @@ const TopRatedDoctors = () => {
           and top-quality surgery facilities right here
         </Typography>
       </Box>
+      <Container sx={{ margin: "30px auto" }}>
+        <Grid container spacing={2}>
+          {doctors.map((doctor: any) => (
+            <Grid item key={doctor.id} md={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="140"
+                  image={doctor.profilePhoto}
+                />
+                
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {doctor.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {doctor.qualification} - {doctor.designation}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Share</Button>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </Box>
   );
 };
